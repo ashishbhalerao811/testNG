987ced6ca3718908e83fc23075bbadbd5d8c6acf
Ashish A. Bhalerao <Ashish A. Bhalerao@192.168.0.4>
2018-12-24 18:30:36 +0530

    Added changes

--- gmailTestNG/src/main/java/sample/CSVReader.java
+++ gmailTestNG/src/main/java/sample/CSVReader.java
@@ -7,32 +7,32 @@ import java.util.List;
 public class CSVReader {
 	public static void main(String [] args) {
 	// This will load csv file 
-	 CSVReader reader = new CSVReader(new FileReader("C:\\Users\\mukesh_otwani\\Desktop\\demo.csv"));
+	// CSVReader reader = new CSVReader(new FileReader("C:\\Users\\mukesh_otwani\\Desktop\\demo.csv"));
 	 
 	 // this will load content into list
-	  List<String[]> li=reader.readAll();
-	  System.out.println("Total rows which we have is "+li.size());
+	 // List<String[]> li=reader.readAll();
+	  //System.out.println("Total rows which we have is "+li.size());
 	            
 	 // create Iterator reference
-	  Iterator<String[]>i1= li.iterator();
+	  //Iterator<String[]>i1= li.iterator();
 	    
 	 // Iterate all values 
-	 while(i1.hasNext()){
+	 //while(i1.hasNext()){
 	     
-	 String[] str=i1.next();
+	 //String[] str=i1.next();
 	   
 	 System.out.print(" Values are ");
-	 
-	 for(int i=0;i<str.length;i++)
-	{
-	 
-	   System.out.print(" "+str[i]);
-	 
-	}
-	   System.out.println("   ");
-	     
-	    
-	}
+//	 
+//	 for(int i=0;i<str.length;i++)
+//	{
+//	 
+//	   System.out.print(" "+str[i]);
+//	 
+//	}
+//	   System.out.println("   ");
+//	     
+//	    
+//	}
 	 
 	}
 	 
